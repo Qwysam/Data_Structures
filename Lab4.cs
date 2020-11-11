3b2c62d2d8ed38ab536232f87595da7238566a14
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2020-11-11 18:48:33 +0200

    Working(kinda)

--- Lab4.cs
+++ Lab4.cs
@@ -6,6 +6,88 @@ namespace Data_Structures
 {
     class Program
     {
+        static int InterpolationSearch(int[] arr, int key,out int Sr,out int Pr)
+        {
+            // Find indexes of 
+            // two corners
+            int lo = 0, hi = (arr.Length - 1);
+            Pr = 2;
+            Sr = 0;
+            // Since array is sorted,  
+            // an element present in 
+            // array must be in range 
+            // defined by corner
+            Sr += 3;
+            while (lo <= hi &&key >= arr[lo] && key <= arr[hi])
+            {
+                Sr++;
+                if (lo == hi)
+                {
+                    Sr++;
+                    if (arr[lo] == key) return lo;
+                    return -1;
+                }
+
+                // Probing the iition  
+                // with keeping uniform  
+                // distribution in mind.
+                double k = (double)(key - arr[lo]) / (arr[hi] - arr[lo]);
+                Pr++;
+                int i = (int)(lo + (hi-lo)*k);
+
+                Pr++;
+                // Condition of  
+                // target found
+                Sr++;
+                if (arr[i] == key)
+                    return i;
+                Sr++;
+                // If x is larger, x 
+                // is in upper part 
+                if (arr[i] < key)
+                    lo = i + 1;
+
+                // If x is smaller, x  
+                // is in the lower part 
+                else {
+                    hi = i - 1;
+                    Pr++;
+                }
+            }
+            return -1;
+        }
+
+        static int BinarySearch(int[] arr,int key,out int Sr, out int Pr)
+        {
+            int min = 0;
+            int max = arr.Length - 1;
+            Pr = 2;
+            Sr = 0;
+            while (min <= max)
+            {
+                Sr++;
+                int mid = (min + max) / 2;
+                Pr++;
+                Sr++;
+                if (key == arr[mid])
+                {
+                    return mid;
+                }
+                else {
+                    if (key < arr[mid])
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+                    Sr++;
+                    Pr++;
+                }
+            }
+            return -1;
+        }
         static int[] ArrayIn(int size)
         {
             int[] arr = new int[size];
@@ -28,103 +110,19 @@ namespace Data_Structures
             Console.WriteLine();
         }
 
-        static void CountingSort(int[] arr, int min, int max)
-        {
-            int[] count = new int[max - min + 1];
-            int z = 0;
-            int comparisons = arr.Length - 1;
-            int exchanges = 0;
-            for (int i = 0; i < count.Length; i++)
-            {
-                count[i] = 0;
-            }
-            for (int i = 0; i < arr.Length; i++)
-            {
-                count[arr[i] - min]++;
-            }
-
-            for (int i = min; i <= max; i++)
-            {
-                while (count[i - min]-- > 0)
-                {
-                    arr[z] = i;
-                    z++;
-                }
-            }
-            exchanges = arr.Length + (arr.Max() - 1) + arr.Length * 2;
-            double[] temp = IntToDouble(arr);
-            Console.WriteLine("Sorted array:");
-            ArrayOutDouble(temp);
-            Console.WriteLine($"{comparisons} comparisons and {exchanges} exchanges have been made");
-            Console.WriteLine($"{comparisons + exchanges} operations in total");
-        }
-
-        static void MSDSort(int[] arr)
-        {
-            int range = 10;
-            int length = 2;
-            int exchanges = arr.Length;
-            int comparisons = 0;
-            ArrayList[] lists = new ArrayList[range];
-            for (int i = 0; i < range; ++i)
-                lists[i] = new ArrayList();
-
-            for (int step = 0; step < length; ++step)
-            {
-                //распределение по спискам
-                for (int i = 0; i < arr.Length; ++i)
-                {
-                    int temp = (arr[i] % (int)Math.Pow(range, step + 1)) / (int)Math.Pow(range, step);
-                    lists[temp].Add(arr[i]);
-                    exchanges++;
-                }
-                //сборка
-                int k = 0;
-                for (int i = 0; i < range; ++i)
-                {
-                    for (int j = 0; j < lists[i].Count; ++j)
-                    {
-                        exchanges++;
-                        arr[k++] = (int)lists[i][j];
-                    }
-                }
-                for (int i = 0; i < range; ++i)
-                    lists[i].Clear();
-            }
-            double[] temp_a = IntToDouble(arr);
-            Console.WriteLine("Sorted array:");
-            ArrayOutDouble(temp_a);
-            Console.WriteLine($"{comparisons} comparisons and {exchanges} exchanges have been made");
-            Console.WriteLine($"{comparisons + exchanges} operations in total");
-        }
-        //Uses both sorting algorithms
-        static void Both(int[] arr)
-        {
-            int[] arr_copy = new int[arr.Length];
-            Array.Copy(arr, arr_copy, arr.Length);
-            Console.WriteLine("\nCount Sort\n");
-            CountingSort(arr, arr.Min(), arr.Max());
-            Console.WriteLine("\nMSD Sort\n");
-            MSDSort(arr_copy);
-        }
-
         //Generates an array using the parameters set by the user
-        public static int[] GenerateArray(int size, int quality, int min, int max)
+        public static int[] GenerateArray(int size, int min, int max)
         {
             Random r = new Random();
             int[] arr = new int[size];
             for (int i = 0; i < size; i++)
                 arr[i] = r.Next(min, max);
-            if (quality != 0)
-                Array.Sort(arr);
-            if (quality == 2)
-                Array.Reverse(arr);
             return arr;
         }
 
         static void Main(string[] args)
         {
-            int size, sort_method, quality, generate;
+            int size, search_method, generate,key;
             Console.Write("Input 'e' to exit\n");
             //main program cycle
             for (; ; )
@@ -137,43 +135,66 @@ namespace Data_Structures
                 if (exit == "e")
                     break;
                 int[] array = new int[size];
-                double[] t = new double[size];
                 //User input of input type
                 Console.WriteLine("Input '0' for manual input or '1' to generate array");
                 int.TryParse(Console.ReadLine(), out generate);
                 if (generate == 0)
                 {
                     //array input
-                    t = ArrayInDouble(size);
-                    array = DoubleToInt(t);
+                    array = ArrayIn(size);
                 }
                 else
                 {
                     //User input of max and min values
-                    int min = 3, max = 100;
-                    //User input of array quality
-                    Console.WriteLine("Input '0' to generate unsorted array, '1' for sorted, '2' for sorted reversed");
-                    int.TryParse(Console.ReadLine(), out quality);
-                    //array generation
-                    array = GenerateArray(size, quality, min, max);
+                    int min = 100, max = 900;
+                    //Console.WriteLine("Input minimal generated number: ");
+                    //int.TryParse(Console.ReadLine(), out min);
+                    //Console.WriteLine("Input maximal generated number: ");
+                    //int.TryParse(Console.ReadLine(), out max);
+                    array = GenerateArray(size, min, max);
+                    ArrayOut(array);
                 }
-                //User input of sorting method
-                Console.WriteLine("Input '1' to use Counting Sort, '2' for MSD Sort, '3' for both");
-                int.TryParse(Console.ReadLine(), out sort_method);
-                //Array output if it has ten or less elements
+                int[] copy = new int[array.Length];
+                Array.Copy(array, copy, array.Length);
+                Array.Sort(copy);
                 if (size <= 10)
                 {
-                    Console.WriteLine("Array:");
-                    double[] temp = IntToDouble(array);
-                    ArrayOutDouble(temp);
+                    Console.WriteLine("Array after sorting:");
+                    ArrayOut(copy);
                 }
+                Console.WriteLine("Input number to search for: ");
+                int.TryParse(Console.ReadLine(), out key);
+                //User input of search method
+                Console.WriteLine("Input '1' to use Binary Search, '2' for Interpolation Search, '3' for C# default search");
+                int.TryParse(Console.ReadLine(), out search_method);
+                //Array output if it has ten or less elements
                 //Choosing sorting method based on user input
-                if (sort_method == 1)
-                    CountingSort(array, array.Min(), array.Max());
-                if (sort_method == 2)
-                    MSDSort(array);
-                if (sort_method == 3)
-                    Both(array);
+                if (search_method == 1)
+                {
+                    int Sr, Pr;
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    int i = BinarySearch(copy, key,out Sr,out Pr);
+                    watch.Stop();
+                    Console.WriteLine($"i = {i}, execution time = "+ watch.Elapsed);
+                    Console.WriteLine($"Compasrisons = {Sr}\tAssignments = {Pr}\t\tSum = {Sr+Pr}");
+                }
+                if(search_method == 2)
+                {
+                    int Sr, Pr;
+                    copy = copy.Distinct().ToArray();
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    int i = InterpolationSearch(copy,key,out Sr,out Pr);
+                    watch.Stop();
+                    Console.WriteLine($"i = {i}, execution time = " + watch.Elapsed);
+                    Console.WriteLine($"Compasrisons = {Sr}\tAssignments = {Pr}\t\tSum = {Sr + Pr}");
+                }
+                if(search_method == 3)
+                {
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    int i = Array.IndexOf(copy,key);
+                    watch.Stop();
+                    Console.WriteLine($"i = {i}, execution time = " + watch.Elapsed);
+                }
             }
         }
     }
